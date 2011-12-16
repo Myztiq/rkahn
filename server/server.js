772b384a780cb630068e136c6aab25b2248344d9
Ryan Kahn <MyztiqPixel@gmail.com>
2011-12-15 19:27:12 -0500

    Updated server configs

--- server/server.js
+++ server/server.js
@@ -7,10 +7,12 @@ var server = express.createServer();
 //server.use(express.logger('tiny'));
 
 server.use(express.vhost('*test.com', testApp));
+
 server.use(express.vhost('*ryan-kahn.com', ryanKahnApp));
 server.use(express.vhost('*ryanakahn.com', ryanKahnApp));
 
-server.use(ryanKahnApp);
+
+server.use(express.vhost('*', ryanKahnApp));
 
 server.listen(8080);
 console.log("Server Started");
