0209273012e557603a05dcc10718f9930e5e3752
Ryan Kahn <MyztiqPixel@gmail.com>
2011-12-18 11:08:32 -0500

    Moved nodE_modules

--- server/server.js
+++ server/server.js
@@ -2,17 +2,18 @@ var express = require('express');
 var server = express.createServer();
 //server.use(express.logger('tiny'));
 
-var chrgy = require('./../../chrgy/server/app.js').app;
-server.use(express.vhost('*chrgy.com', chrgy));
-server.use(express.vhost('*chr.gy', chrgy));
-
-var kirk = require('./../../kirk/server/app.js').app;
-server.use(express.vhost('*kirksteineck.com', kirk));
-
 var ryanKahnApp = require('./app').app;
 server.use(express.vhost('*ryan-kahn.com', ryanKahnApp));
 server.use(express.vhost('*ryanakahn.com', ryanKahnApp));
 server.use(express.vhost('*', ryanKahnApp));
+
+var chrgy = require('./../../chrgy/server/app.js').app;
+server.use(express.vhost('*chrgy.com', chrgy));
+server.use(express.vhost('*chr.gy', chrgy));
+
+//var kirk = require('./../../kirk/server/app.js').app;
+//server.use(express.vhost('*kirksteineck.com', kirk));
+
 
 server.listen(8080);
 console.log("All Servers Started");
