f0a2bcbd8e36ec74672fa09d9235adb64425ac7d
Ryan Kahn <MyztiqPixel@gmail.com>
2011-12-18 10:44:50 -0500

    Updated modules

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 var express = require('express');
-
-var ryanKahnApp = require('./app').app;
-var chrgy = require('./../../chrgy/server/app.js').app;
 var server = express.createServer();
-
 //server.use(express.logger('tiny'));
 
+var chrgy = require('./../../chrgy/server/app.js').app;
 server.use(express.vhost('*chrgy.com', chrgy));
 server.use(express.vhost('*chr.gy', chrgy));
 
+var kirk = require('./../../kirk/server/app.js').app;
+server.use(express.vhost('*kirksteineck.com', kirk));
+
+var ryanKahnApp = require('./app').app;
 server.use(express.vhost('*ryan-kahn.com', ryanKahnApp));
 server.use(express.vhost('*ryanakahn.com', ryanKahnApp));
-
-
 server.use(express.vhost('*', ryanKahnApp));
 
 server.listen(8080);
