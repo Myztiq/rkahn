2bf47946944928b0b91ce0f8e5b70dd02d0efc73
Ryan Kahn <MyztiqPixel@gmail.com>
2012-03-06 22:39:38 -0500

    "Updated index page"

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-
 var defaultPort = 83;
-if(process.argv.length > 1 && process.argv[2].length){
+console.log(process.argv);
+if(process.argv && process.argv.length > 1 && process.argv[2].length){
 	require('./app').app.listen(process.argv[2]);
 	console.log("Started on port: "+process.argv[2]);
 }else{
