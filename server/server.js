0dba2da437ad3bd5cf9261678a74fe80465493ad
Ryan Kahn <MyztiqPixel@gmail.com>
2012-03-06 22:40:37 -0500

    "Updated server.js"

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 var defaultPort = 83;
-console.log(process.argv);
-if(process.argv && process.argv.length > 1 && process.argv[2].length){
-	require('./app').app.listen(process.argv[2]);
-	console.log("Started on port: "+process.argv[2]);
+if(process.argv && process.argv.length > 2 && process.argv[2].length){
+  require('./app').app.listen(process.argv[2]);
+  console.log("Started on port: "+process.argv[2]);
 }else{
-	require('./app').app.listen(defaultPort);
-	console.log("Started on port: "+defaultPort);
+  require('./app').app.listen(defaultPort);
+  console.log("Started on port: "+defaultPort);
 }
